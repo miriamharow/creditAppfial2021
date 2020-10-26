ab96f99dad7c0474651d063892bfd01e79166e83
miriam <msh3491@gmaul.com>
2020-10-26 15:13:08 +0200

    Initiate

---
+++ creditApp/app/src/main/java/com/miriam_shmuel/creditapp/LogInActivity.java
@@ -0,0 +1,12 @@
+package com.miriam_shmuel.creditapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogInActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
