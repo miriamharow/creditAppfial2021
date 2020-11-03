377e66b88f30e988f3b05f87091df011501ed083
Samuel Nakache <samuel.nakache93@gmail.com>
2020-11-03 15:15:30 +0200

    Add show/hide password

--- creditApp/app/src/main/java/com/miriam_shmuel/creditapp/LogInActivity.java
+++ creditApp/app/src/main/java/com/miriam_shmuel/creditapp/LogInActivity.java
@@ -3,9 +3,12 @@ package com.miriam_shmuel.creditapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -109,4 +112,25 @@ public class LogInActivity extends Activity {
     public void onDestroy() {
         super.onDestroy();
     }
+
+    public void ShowHidePass(View view){
+
+        if(view.getId()==R.id.show_pass_btn){
+
+            if(password.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())){
+                ((ImageView)(view)).setImageResource(R.drawable.hide_password);
+
+                //Show Password
+                password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }
+            else{
+                ((ImageView)(view)).setImageResource(R.drawable.show_password);
+
+                //Hide Password
+                password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+            }
+        }
+    }
 }
+
