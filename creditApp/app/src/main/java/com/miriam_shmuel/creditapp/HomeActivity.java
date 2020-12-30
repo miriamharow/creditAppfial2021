47d3630f116c4c040e69efd21cb2a53c9e96d0f7
miriam <msh3491@gmaul.com>
2020-12-30 10:06:56 +0200

    menu exit & about

--- creditApp/app/src/main/java/com/miriam_shmuel/creditapp/HomeActivity.java
+++ creditApp/app/src/main/java/com/miriam_shmuel/creditapp/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.miriam_shmuel.creditapp;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -23,7 +26,7 @@ import com.google.firebase.auth.FirebaseAuth;
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_home);
-           //--------------------SCREEN-------------------------
+            //--------------------SCREEN-------------------------
             getSupportActionBar().setTitle("My Credit");
             getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00264d")));
 
@@ -42,21 +45,48 @@ import com.google.firebase.auth.FirebaseAuth;
             });
             //---------------------------------------------------
 
-            //--------------------LOG OUT------------------------
-            btnLogout = findViewById(R.id.logout);
-            btnLogout.setOnClickListener(new View.OnClickListener() {
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            super.onCreateOptionsMenu(menu);
+            MenuItem menuAbout = menu.add("About");
+            MenuItem menuExit = menu.add("Exit");
+
+            menuAbout.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
                 @Override
-                public void onClick(View v) {
-                    Toast.makeText(HomeActivity.this,"log out!",Toast.LENGTH_SHORT).show();
-                    FirebaseAuth.getInstance().signOut();
-                    finish();
-                    Intent intToLogIn = new Intent(HomeActivity.this, LogInActivity.class);
-                    startActivity(intToLogIn);
+                public boolean onMenuItemClick(MenuItem item) {
+                    showAboutDialog();
+                    return true;
                 }
             });
-            //---------------------------------------------------
+
+            menuExit.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    showExitDialog();
+                    return true;
+                }
+            });
+            return true;
+        }
+
+        private void showAboutDialog() {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("About CreditApp");
+            alertDialog.setMessage("This app implements the CreditApp.\n\nBy Miriam Harow & Samuel Nakash (c)");
+            alertDialog.show();
+        }
+
+        private void showExitDialog() {
+            Toast.makeText(HomeActivity.this, "log out!", Toast.LENGTH_SHORT).show();
+            FirebaseAuth.getInstance().signOut();
+            finish();
+            Intent intToLogIn = new Intent(HomeActivity.this, LogInActivity.class);
+            startActivity(intToLogIn);
         }
     }
+
 
 
 
