071c695b681592e8024069acaefaf10e674700a5
miriam <msh3491@gmaul.com>
2021-01-15 00:30:21 +0200

    start fregment

---
+++ creditApp/app/src/main/java/com/miriam_shmuel/creditapp/Adaptercredits.java
@@ -0,0 +1,67 @@
+package com.miriam_shmuel.creditapp;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class Adaptercredits extends ArrayAdapter<Gift_Credit> {
+    /*
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     **/
+    Context context; // The current context. Used to inflate the layout file.
+    ArrayList <Gift_Credit> creditlist; //A List of AndroidFlavor objects to display in a list
+    int resource; // layout file
+
+    public Adaptercredits(Context context, int resource, ArrayList<Gift_Credit> contactlist) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // this is a custom adapter for two TextViews and an ImageView not
+        super(context, resource, contactlist);
+        this.context = context;
+        this.resource = resource;
+        this.creditlist = contactlist;
+        Log.d("Debug", "getview constractor v" );
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view. like index
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+
+    @Nullable
+    @Override
+    public View getView(int position, @Nullable View convertView, @Nullable ViewGroup parent)
+    {
+        Log.d("Debug", "getview  v" );
+        // get view
+       LayoutInflater layoutInflater = LayoutInflater.from(context);
+       View view = layoutInflater.inflate(R.layout.item_element, parent, false);
+
+        // bind to view elements
+        TextView cNameStore = (TextView)view.findViewById(R.id.cNameID);
+        TextView cDate = (TextView)view.findViewById(R.id.CphoneID);
+        TextView cbarCode = (TextView)view.findViewById(R.id.CbarbarcodeID);
+
+        // add contact to list in specific position
+        Gift_Credit creditI = creditlist.get(position);
+        cNameStore.setText(creditI.getShopName().get(0).getName());
+        cDate.setText(creditI.getExpirationDate());
+        cbarCode.setText(creditI.getBarCode());
+
+        return view;
+    }
+}
--- creditApp/app/src/main/java/com/miriam_shmuel/creditapp/CreditFragment.java
+++ creditApp/app/src/main/java/com/miriam_shmuel/creditapp/CreditFragment.java
@@ -1,27 +1,56 @@
 package com.miriam_shmuel.creditapp;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class CreditFragment extends Fragment {
+    public View view;
+    private List_of_Credits listCredit;
+    private Adaptercredits cAdpter;
+    private ListView listView;
+    private ArrayList<Gift_Credit> arrayList;
 
     public CreditFragment() {
-// Required empty public constructor
+    // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //-----------------ADAPTER LIST VIEW-----------------
+        // initialize Adapter
+        //arrayList = listCredit.getListOfCredit();
+        Shop s = new Shop("Zara");
+        ArrayList<Shop> shopName = new ArrayList<Shop> ();
+        shopName.add(s);
+
+        Gift_Credit c = new Gift_Credit("1234", "1234", "1/2/3", shopName, "c");
+        arrayList = new  ArrayList<>();
+
+        cAdpter = new Adaptercredits(getContext(), R.layout.item_element, arrayList);
+        view = getLayoutInflater().inflate(R.layout.fragment_credit, null);
+        listView = view.findViewById(R.id.listViewID);
+        listView.setAdapter(cAdpter);
+
+        arrayList.add(c);
+        cAdpter.notifyDataSetChanged();
+        //---------------------------------------------------
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-// Inflate the layout for this fragment
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_credit, container, false);
     }
 
