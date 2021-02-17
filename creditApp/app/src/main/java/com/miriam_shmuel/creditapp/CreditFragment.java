fbd5d80ceceda5f80958edb2e17bd248414bd6ab
Samuel Nakache <samuel.nakache93@gmail.com>
2021-02-17 15:16:28 +0200

    Test go to editItem

--- creditApp/app/src/main/java/com/miriam_shmuel/creditapp/CreditFragment.java
+++ creditApp/app/src/main/java/com/miriam_shmuel/creditapp/CreditFragment.java
@@ -1,11 +1,14 @@
 package com.miriam_shmuel.creditapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -30,11 +33,13 @@ public class CreditFragment extends Fragment  {
     private Map<String, Object> data;
     public View view;
     private AdapterCreditsGifts cAdpter;
-    private ListView listView;
-    private ArrayList<Gift_Credit> arrayList;
+    public ListView listView;
+    public ArrayList<Gift_Credit> arrayList;
     private FirebaseFirestore db;
     private FirebaseUser user;
     private String email;
+
+
 
     public CreditFragment() {
         user = FirebaseAuth.getInstance().getCurrentUser();
@@ -43,58 +48,63 @@ public class CreditFragment extends Fragment  {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        //------------------OPEN ITEM------------------------
-        /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                Gift_Credit credit = arrayList.get(position);
-                String shopname = credit.getShopName().get(0).toString();
-                String value = credit.getValue();
-                String expDate = credit.getExpirationDate();
-                /*Intent intentCI = new Intent(MainActivity.this, CreditItem.class);
-                intentCI.putExtra("storeName",sn);
-                intentCI.putExtra("expDate", ed);
-                intentCI.putExtra("numberBarCode", nbc);
-                intentCI.putExtra("cis", isc);
-                startActivity(intentCI);*/
-            //}
-        //});
-
-        //---------------------------------------------------
-
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         final View view = inflater.inflate(R.layout.fragment_credit, container, false);
         arrayList = new  ArrayList<>();
+        listView = (ListView)view.findViewById(R.id.listViewID);
         CollectionReference ColRef = db.collection("user").document(email).collection("list of credit");
         //asynchronously retrieve all documents
+        Log.d("test","in 0");
         ColRef.get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        Log.d("test","in list");
                         if (task.isSuccessful()) {
+                            int i=0;
+                            Log.d("test","in 1");
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 arrayList.add(document.toObject(Gift_Credit.class));
+                                Log.d("test","number array "+i++);
                             }
                             cAdpter = new AdapterCreditsGifts(getActivity(), R.layout.item_element, arrayList);
                             //view = getLayoutInflater().inflate(R.layout.fragment_credit, null);
-                            listView = (ListView)view.findViewById(R.id.listViewID);
                             listView.setAdapter(cAdpter);
+                            Log.d("test","in 1.1");
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+                            {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+                                {
+                                    Log.d("test","in 2");
+                                    Log.d("test","clicked");
+                                    //Gift_Credit credit = arrayList.get(position);
+                                    Intent intentCI = new Intent(getActivity(), EditItemActivity.class);
+                                    startActivity(intentCI);
+                                }
+                            });
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
                         }
+
                     }
                 });
 
+        Log.d("test","in 3");
+        Toast.makeText(getActivity(), ""+arrayList.isEmpty(), Toast.LENGTH_SHORT).show();
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//        {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+//            {
+//                Log.d("test","clicked");
+//                //Gift_Credit credit = arrayList.get(position);
+//                Intent intentCI = new Intent(getActivity(), EditItemActivity.class);
+//                startActivity(intentCI);
+//            }
+//        });
         return view;
     }
 
